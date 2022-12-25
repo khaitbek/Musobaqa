f7c7afb763c4a7db226f535d34825984328e352b
khaitbek <khaitbek2005@gmail.com>
2022-12-25 22:25:55 +0500

    finish

--- js/app.js
+++ js/app.js
@@ -8,9 +8,26 @@ const loginDialog = document.querySelector("#loginDialog");
 const closeDialogBtn = document.querySelector("#closeDialog");
 const passwordInput = document.querySelector("#userPassword");
 const showPasswordBtn = document.querySelector("#showPasswordBtn");
+const overlay = document.querySelector("#overlay");
 
 
-if (currentTheme === "dark") document.body.classList.add("dark");
+if (currentTheme === "dark") {
+    document.body.classList.add("dark");
+    changeBrandImages();
+};
+
+function changeBrandImages(){
+    const darkModeBrandImages = ["../images/brands/ford-dark.svg","../images/brands/jeep-dark.svg","../images/brands/dodge-dark.svg","../images/brands/nissan-dark.svg","../images/brands/toyota-dark.svg","../images/brands/hyundai-dark.svg"];
+    const lightModeBrandImages = ["../images/brands/ford.png","../images/brands/jeep.png","../images/brands/dodge.png","../images/brands/nissan.png","../images/brands/toyota.png","../images/brands/hyundai.png"];
+    const brandImagesToRender = window.localStorage.getItem("theme") !== "dark" ? [...lightModeBrandImages] : [...darkModeBrandImages];
+    const allBrands = document.querySelectorAll("#brandItem");
+    console.log(allBrands);
+    allBrands.forEach((brand,index) => {
+        const brandImg = brand.querySelector("img");
+        brandImg.src = brandImagesToRender[index];
+        console.log(brandImg);
+    })
+}
 
 function closeDialog(){
     loginDialog.close();
@@ -31,20 +48,24 @@ closeDialogBtn.addEventListener("click", ()=>{
     closeDialog();
 })
 hamburgerBtn.addEventListener("click", () => {
-    document.body.classList.add("overlay");
+    overlay.classList.add("show");
     navigation.classList.add("open");
 });
 closeNavBtn.addEventListener("click", () => {
-    document.body.classList.remove("overlay");
+    overlay.classList.remove("show");
+
     navigation.classList.remove("open");
 });
 themeToggler.addEventListener("click", () => {
-    console.log(localStorage.getItem("theme"));
     if (localStorage.getItem("theme") === "dark") {
         document.body.classList.remove("dark");
         localStorage.removeItem("theme");
+        changeBrandImages();
         return
     };
-    document.body.classList.add("dark");
     localStorage.setItem("theme", "dark");
+    document.body.classList.add("dark");
+    setTimeout(()=>{
+        changeBrandImages();
+    },1000);
 })
--- js/secondCarousel.js
+++ js/secondCarousel.js
@@ -3,7 +3,7 @@ $('.card__list').slick({
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
-  dots:true,
+  dots: true,
   responsive: [
     {
       breakpoint: 1400,
@@ -18,12 +18,29 @@ $('.card__list').slick({
       }
     },
   ],
-  dotsClass:"slick-dots-mansur", 
+  dotsClass: "slick-dots-mansur",
   customPaging: (slider, i) => {
     return "<button class='slick-dot-mansur' type='button'> </button>"
-},
+  }
 
 });
+
+$('.card__list').on("afterChange",function(){
+  const dots = document.querySelectorAll(".slick-dot-mansur");
+  const cards = document.querySelectorAll(".card__item");
+  cards.forEach((card,index) => {
+    if(card.classList.contains("slick-current")){
+      dots.forEach(dot => {
+        dot.classList.remove("active-dot");
+      })
+      if(index >= dots.length){
+        dots[index - dots.length + 1].classList.add("active-dot");
+        return
+      }
+      dots[index].classList.add("active-dot");
+    }
+  })
+})
 
 // $('.card__list').slick({
 //   slidesToShow: 3.7,
